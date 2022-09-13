c0f8b9a8553039fc20dbce44785177bab8cb3a80
kim-nz <107521992+kim-nz@users.noreply.github.com>
2022-09-14 11:33:36 +1200

    add auth id to join

--- server/db/dashboard.js
+++ server/db/dashboard.js
@@ -5,6 +5,7 @@ function getAllCaptionedImages(db = connection) {
     .join('captions', 'images.id', 'captions.image_id')
     .select(
       'images.id as imageId',
+      'captions.auth0_id as auth0ImageId',
       'captions.id as captionId',
       'image_url as imageUrl',
       'caption_text as captionText'

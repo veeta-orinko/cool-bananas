1ebd5ffad246abdffae337a46232ee61dcd6e79a
Bleuette <102277517+BleuHere@users.noreply.github.com>
2022-09-14 11:53:36 +1200

    add a route to get a tag by parameter, test this function

---
+++ server/routes/__tests__/tagged.test.js
@@ -0,0 +1,52 @@
+const request = require('supertest')
+const server = require('../../server')
+
+const { getAllByTag } = require('../../db/tagged')
+jest.mock('../../db/tagged')
+
+jest.spyOn(console, 'error')
+
+afterEach(() => {
+  console.error.mockReset()
+})
+
+describe('GET /api/v1/tagged/:tag', () => {
+  const fakeObjectArray = [
+    {
+      id: 1,
+      auth0_id: '1',
+      image_url: '/images/banana0.png',
+      tags: 'banana, coolio, pyjamas',
+    },
+    {
+      id: 2,
+      auth0_id: '1',
+      image_url:
+        'https://pbs.twimg.com/media/ErKJ6RwVgAE06ET?format=jpg&name=small',
+      tags: 'coolio, Yu-Gi-Oh, finger',
+    },
+  ]
+  it('returns status 200 and an array of objects when db function resolves', () => {
+    getAllByTag.mockReturnValue(Promise.resolve(fakeObjectArray))
+    return request(server)
+      .get('/api/v1/tagged/coolio')
+      .then((res) => {
+        expect(res.status).toBe(200)
+        expect(res.body).toHaveLength(2)
+        expect(res.body[0].image_url).toBe('/images/banana0.png')
+        return null
+      })
+  })
+  it('returns status 500 and an error message when db function rejects', () => {
+    getAllByTag.mockImplementation(() =>
+      Promise.reject(new Error('oh dear, sad'))
+    )
+    return request(server)
+      .get('/api/v1/tagged/coolio')
+      .then((res) => {
+        expect(res.status).toBe(500)
+        expect(res.text).toBe('oh dear, sad')
+        return null
+      })
+  })
+})
--- server/routes/tagged.js
+++ server/routes/tagged.js
@@ -2,14 +2,18 @@ const express = require('express')
 
 const router = express.Router()
 
+const db = require('../db/tagged')
+
 // GET /api/v1/tagged/
 router.get('/', (req, res) => {
   res.send('tagged route hit!')
 })
 
 router.get('/:tag', (req, res) => {
-  // what the user has searched for
-  res.send(`tagged route hit! with: ${req.params.tag}`)
+  const { tag } = req.params
+  db.getAllByTag(tag)
+    .then((response) => res.json(response))
+    .catch((err) => res.status(500).send(err.message))
 })
 
 module.exports = router

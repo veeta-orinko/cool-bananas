8ed7019a25195a92515e5c25e671f0ebdaa069a8
Hamish Mourie <20253849+hamish-mourie@users.noreply.github.com>
2022-09-14 11:43:33 +1200

    Add navbar login features

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,15 +1,28 @@
 import React, { useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useAuth0 } from '@auth0/auth0-react'
+import { IfAuthenticated, IfNotAuthenticated } from './Authenicated'
 
 import { clearUser, setUser } from '../actions/user'
 import { getUser } from '../apis/users'
 
 export default function Navbar() {
+  const { isAuthenticated, getAccessTokenSilently, logout, loginWithRedirect } =
+    useAuth0()
   const dispatch = useDispatch()
   const navigate = useNavigate()
-  const { isAuthenticated, getAccessTokenSilently } = useAuth0()
+  const user = useSelector((state) => state.loggedInUser)
+
+  const handleLogOff = (e) => {
+    e.preventDefault()
+    logout()
+  }
+
+  const handleSignIn = (e) => {
+    e.preventDefault()
+    loginWithRedirect()
+  }
 
   useEffect(() => {
     if (!isAuthenticated) {
@@ -26,12 +39,20 @@ export default function Navbar() {
 
   return (
     <nav className='navbar'>
-      HERE IS THE NAVBAR
       <Link to='/dashboard'>Dashboard</Link>
       <Link to='/collection'>My Collection</Link>
       <Link to='/tagged'>Search by tag</Link>
       <Link to='/create'>Create</Link>
-      Register | Login | Log out
+      <Link to='/register'>Register</Link>
+      <IfAuthenticated>
+        <p>{' ' + user.name}</p>
+      </IfAuthenticated>
+      <Link to='/' onClick={handleSignIn}>
+        Login
+      </Link>
+      <Link to='/' onClick={handleLogOff}>
+        Log out
+      </Link>
     </nav>
   )
 }

a8aca13d4c88a42f960ebdf379163297c5d12603
Max (Amber) Hilton <107532278+max-is-coding@users.noreply.github.com>
2022-09-14 10:44:41 +1200

    AStarted on authentication, added authenticated file

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { useAuth0 } from '@auth0/auth0-react'
+
+import { clearUser, setUser } from '../actions/user'
 
 export default function Navbar() {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const { isAuthenticated, getAccessTokenSilently } = useAuth0()
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      dispatch(clearUser())
+    } else {
+      getAccessTokenSilently()
+        .then((token) => getUser(token))
+        .then((userInDb) => {
+          userInDb ? dispatch(setUser(userInDb)) : navigate('/register')
+        })
+        .catch((err) => console.error(err))
+    }
+  }, [isAuthenticated])
+
   return (
     <nav className='navbar'>
       HERE IS THE NAVBAR

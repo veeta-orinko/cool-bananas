a51b9336737088defdb878cf1ab6217d0f18e12d
Grant Scott-Hayes <107601640+grant-scott-hayes@users.noreply.github.com>
2022-09-14 15:00:17 +1200

    add tests for getallimages and passed

--- server/routes/__tests__/tagged.test.js
+++ server/routes/__tests__/tagged.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest')
 const server = require('../../server')
 
-const { getAllImagesByTag } = require('../../db/tagged')
+const { getAllImagesByTag, getAllImages } = require('../../db/tagged')
 jest.mock('../../db/tagged')
 
 jest.spyOn(console, 'error')
@@ -10,30 +10,40 @@ afterEach(() => {
   console.error.mockReset()
 })
 
+const fakeObjectArray = [
+  {
+    captionId: 1,
+    captionText: 'Banananana',
+    imageId: 1,
+    imageUrl: '/images/banana0.png',
+    tags: 'banana, cool, pyjamas',
+  },
+  {
+    captionId: 2,
+    captionText: "When you're a happy banana",
+    imageId: 1,
+    imageUrl: '/images/banana0.png',
+    tags: 'banana, cool, pyjamas',
+  },
+  {
+    captionId: 3,
+    captionText: 'Cool bananas',
+    imageId: 1,
+    imageUrl: '/images/banana0.png',
+    tags: 'banana, cool, pyjamas',
+  },
+]
+
 describe('GET /api/v1/tagged/:tag', () => {
-  const fakeObjectArray = [
-    {
-      id: 1,
-      auth0_id: '1',
-      image_url: '/images/banana0.png',
-      tags: 'banana, coolio, pyjamas',
-    },
-    {
-      id: 2,
-      auth0_id: '1',
-      image_url:
-        'https://pbs.twimg.com/media/ErKJ6RwVgAE06ET?format=jpg&name=small',
-      tags: 'coolio, Yu-Gi-Oh, finger',
-    },
-  ]
   it('returns status 200 and an array of objects when db function resolves', () => {
     getAllImagesByTag.mockReturnValue(Promise.resolve(fakeObjectArray))
     return request(server)
       .get('/api/v1/tagged/coolio')
       .then((res) => {
         expect(res.status).toBe(200)
-        expect(res.body).toHaveLength(2)
-        expect(res.body[0].image_url).toBe('/images/banana0.png')
+        expect(res.body).toHaveLength(3)
+        expect(res.body[0].imageUrl).toBe('/images/banana0.png')
+        expect(res.body[2].captionText).toBe('Cool bananas')
         return null
       })
   })
@@ -46,6 +56,33 @@ describe('GET /api/v1/tagged/:tag', () => {
       .then((res) => {
         expect(res.status).toBe(500)
         expect(res.text).toBe('oh dear, sad')
+        return null
+      })
+  })
+})
+
+describe('GET /api/v1/tagged/', () => {
+  it('returns status 200 and an array of objects when db function resolves', () => {
+    getAllImages.mockReturnValue(Promise.resolve(fakeObjectArray))
+    return request(server)
+      .get('/api/v1/tagged/')
+      .then((res) => {
+        expect(res.status).toBe(200)
+        expect(res.body).toHaveLength(3)
+        expect(res.body[0].imageUrl).toBe('/images/banana0.png')
+        expect(res.body[2].captionText).toBe('Cool bananas')
+        return null
+      })
+  })
+  it('returns status 500 and an error message when db function rejects', () => {
+    getAllImages.mockImplementation(() =>
+      Promise.reject(new Error('Golly gee-wizz, I am sad'))
+    )
+    return request(server)
+      .get('/api/v1/tagged/')
+      .then((res) => {
+        expect(res.status).toBe(500)
+        expect(res.text).toBe('Golly gee-wizz, I am sad')
         return null
       })
   })

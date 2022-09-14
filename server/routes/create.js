c8e777980c61c6e4cd5ae0b6bad310d9c8aae5dd
Jeremy-Hunter <19340214+jeremy-hunter3692@users.noreply.github.com>
2022-09-14 12:08:06 +1200

    addImageCaption post working

--- server/routes/create.js
+++ server/routes/create.js
@@ -1,10 +1,25 @@
 const express = require('express')
 
+const db = require('../db/create')
+
 const router = express.Router()
 
 // GET /api/v1/create/
+
 router.get('/', (req, res) => {
   res.send('create route hit!')
+})
+
+router.post('/', (req, res) => {
+  const caption = req.body
+  db.addImageCaption(caption)
+    .then((ids) => {
+      res.json({ ...caption, id: ids[0] })
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({ message: 'Something bad happened' })
+    })
 })
 // post route
 

d2d60b9a1d77fb198879383cbde865296323f801
Veeta Orinko <107618223+veeta-orinko@users.noreply.github.com>
2022-09-14 16:14:28 +1200

    some testing has been added to the collection database

---
+++ server/db/__tests__/collection.test.js
@@ -0,0 +1,32 @@
+const knex = require('knex')
+const { getImagesByUserId } = require('../collection')
+const testConfig = require('../knexfile').test
+const testDb = knex(testConfig)
+
+// // const { getCaptionedImages } = require('../landing')
+// const db = require('./db/collection.js')
+
+beforeAll(() => {
+  return testDb.migrate.latest()
+})
+
+beforeEach(() => {
+  return testDb.seed.run()
+})
+
+afterAll(() => {
+  return testDb.destroy()
+})
+
+describe('getImage', () => {
+  it('checks the id, auth0_id, URL and image count', () => {
+    const userId = '1'
+    expect.assertions(4)
+    return getImagesByUserId(userId, testDb).then((images) => {
+      expect(images[0].id).toBe(1)
+      expect(images[0].auth0_id).toBe('1')
+      expect(images[0].image_url).toBe('/images/banana0.png')
+      expect(images).toHaveLength(2)
+    })
+  })
+})

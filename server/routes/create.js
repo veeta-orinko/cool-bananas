01efa8f6a1fc9ce60fb911f105a147d86c72128c
Jeremy-Hunter <19340214+jeremy-hunter3692@users.noreply.github.com>
2022-09-14 16:20:37 +1200

    routes not working

--- server/routes/create.js
+++ server/routes/create.js
@@ -11,16 +11,70 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res) => {
+  const url = req.body[0] // "{url: http://www.jfklea.com/jkfl;aewafdajkl;}"
   const caption = req.body
-  db.addImageCaption(caption)
+  // const caption = req.body.caption
+  // console.log(req.body)
+  //   {
+  //     "caption": { "captionText": "brand new caption", "auth0Id": 2},
+  //     "url": { "imageUrl": "http://www.jfklea.com/jkfl;aewafdajkl",
+  // "auto0Id": 2 }
+  // //  }
+  const newCaption = {
+    caption_text: caption.captionText,
+    auth0_id: caption.auth0Id,
+  }
+
+  db.addImageUrl(url)
     .then((ids) => {
-      res.json({ ...caption, id: ids[0] })
+      return db.addImageCaption({ ...newCaption, image_id: ids[0] })
+    })
+    .then((ids) => {
+      res.json({ ...newCaption, id: ids[0] })
     })
     .catch((err) => {
       console.log(err)
       res.status(500).json({ message: 'Something bad happened' })
     })
 })
+
+// router.post('/', checkJwt, (req, res) => {
+//   const { fruit } = req.body
+//   const auth0Id = req.user?.sub
+//   const newFruit = {
+//     added_by_user: auth0Id,
+//     name: fruit.name,
+//     average_grams_each: fruit.averageGramsEach,
+//   }
+
+//   db.addFruit(newFruit)
+//     .then(() => db.getFruits())
+//     .then((fruits) => res.json({ fruits }))
+//     .catch((err) => {
+//       console.error(err)
+//       res.status(500).send(err.message)
+//     })
+// })
+
+// router.post('/', (req, res) => {
+//   const url = req.body
+//   console.log('url: ', url)
+//   db.addImageUrl(url)
+//     .then((ids) => {
+//       res.json({ ...url, id: ids[0] })
+//     })
+//     .catch((err) => {
+//       console.log(err)
+//       res.status(500).json({ message: 'Something bad happened' })
+//     })
+// })
+// post
+
 // post route
 
+// {
+//   "auth0_id": 2,
+//   "image_url": "https://i.imgur.com/yX4444wNrUV.jpeg",
+//   "tags": "second comic strip"
+// }
 module.exports = router

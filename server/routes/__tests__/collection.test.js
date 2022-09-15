1b1f7994184df2d275eb10cb4d94a951f316bd3b
peter-pakoti <107296263+peter-pakoti@users.noreply.github.com>
2022-09-15 16:39:39 +1200

    added additional test for catch error

--- server/routes/__tests__/collection.test.js
+++ server/routes/__tests__/collection.test.js
@@ -4,6 +4,12 @@ const server = require('../../server')
 
 jest.mock('../../db/collection')
 
+jest.spyOn(console, 'error')
+
+afterEach(() => {
+  console.error.mockReset()
+})
+
 describe('GET /api/v1/collection', () => {
   test('gets correct images', () => {
     db.getImagesByUserId.mockReturnValue(
@@ -27,6 +33,20 @@ describe('GET /api/v1/collection', () => {
       .then((response) => {
         expect(response.body).toHaveLength(2)
         expect(response.body[0].image_url).toBe('/images/apple.png')
+      })
+  })
+  test('should return status 500 and an error message when database fails.', () => {
+    expect.assertions(3)
+    db.getImagesByUserId.mockImplementation(() =>
+      Promise.reject(new Error('Test error message'))
+    )
+    console.error.mockImplementation(() => {})
+    return request(server)
+      .get('/api/v1/collection/')
+      .then((res) => {
+        expect(res.status).toBe(500)
+        expect(res.text).toContain('Server error')
+        expect(console.error).toHaveBeenCalledWith('Test error message')
       })
   })
 })

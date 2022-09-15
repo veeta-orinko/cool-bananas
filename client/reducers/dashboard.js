6c1729abfe0b8ce946644ca1e32c1cd056bc5050
Eli Bing <107372513+elizabeth-bing@users.noreply.github.com>
2022-09-15 17:56:55 +1200

    dashboard reducer test and action test passed

--- client/reducers/dashboard.js
+++ client/reducers/dashboard.js
@@ -2,11 +2,11 @@ import { SET_DASHBOARD_CONTENT } from '../actions/dashboard'
 
 const initialState = [
   {
-    imageId: 1,
-    auth0ImageId: '1',
-    captionId: 1,
-    imageUrl: '/images/banana0.png',
-    captionText: 'Banananana',
+    // imageId: 1,
+    // auth0ImageId: '1',
+    // captionId: 1,
+    // imageUrl: '/images/banana0.png',
+    // captionText: 'Banananana',
   },
 ]
 

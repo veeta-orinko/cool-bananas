0aa2dccd94ec4986d5597926a9a34be1237e75c5
david-kavenga <david.kavenga@gmail.com>
2022-09-16 14:36:15 +1200

    Fix for linting

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,18 +2,12 @@ import { combineReducers } from 'redux'
 
 import landing from './landing'
 import user from './user'
-<<<<<<< HEAD
 import dashboard from './dashboard'
-=======
 import collection from './collection'
->>>>>>> 49ba0281ec405f57418437307fd9750699091576
 
 export default combineReducers({
   landing: landing,
   user: user,
-<<<<<<< HEAD
   dashboard: dashboard,
-=======
   collection: collection,
->>>>>>> 49ba0281ec405f57418437307fd9750699091576
 })

1929600ac18ceaf4b41721d4f79c70ef64e87fef
Eli Bing <107372513+elizabeth-bing@users.noreply.github.com>
2022-09-16 12:31:09 +1200

    api test 1 working

---
+++ client/apis/__tests__/dashboard.test.js
@@ -0,0 +1,39 @@
+import nock from 'nock'
+import { getDashboardContent } from '../dashboard'
+
+const dashboardContentMockData = [
+  {
+    imageId: 1,
+    auth0ImageId: '1',
+    captionId: 1,
+    imageUrl: '/mockImageUrl',
+    captionText: 'mockCaptionText',
+  },
+  {
+    imageId: 3,
+    auth0ImageId: '3',
+    captionId: 3,
+    imageUrl: '/mockImageUrl3',
+    captionText: 'mockCaptionText3',
+  },
+]
+
+describe('GET /api/v1/dashboard', () => {
+  it('gets the dashboard page content', async () => {
+    expect.assertions(1)
+    const scope = nock('http://localhost')
+      .get('/api/v1/dashboard')
+      .reply(200, dashboardContentMockData)
+
+    const dashboardContent = await getDashboardContent()
+    expect(dashboardContent).toEqual(dashboardContentMockData)
+    scope.done()
+  })
+})
+
+describe('shuffle function /api/v1/dashboard', () => {
+  it('shuffle function works and returns a random image', async () => {
+    expect.assertions(1)
+    const scope = nock('http://localhost')
+  })
+})
